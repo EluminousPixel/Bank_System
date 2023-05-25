5c4ab14d60d67cf78e69c714d77e7c30b1f2e482
Pixel <110355772+EluminousPixel@users.noreply.github.com>
2023-05-25 15:22:09 +0100

    Update 1.1

--- some_new_work.py
+++ some_new_work.py
@@ -1,21 +1,30 @@
+#List of dictionaries with users log-in info
 name_gp1 = {
     "Username": ["Pix",], 
     "Password": ["jUmP?"],
-    "Money": 120
+    "Money": 120,
+    "Code": 2385
 }
 
 name_gp2 = {
     "Username": ["Jin"], 
-    "Password": ["DuCkErS!"]
+    "Password": ["DuCkErS!"],
+    "Money": 54,
+    "Code": 6937
+
 }
 
 name_gp3 = {
     "Username": [""], 
-    "Password": [""]
-}
+    "Password": [""],
+    "Money": 0,
+    "Code": []
+    
+    }
 
 u = ""
 
+#The basic log-in feature which scans the dicts to see if the username and passwords are there.
 def log_in():
     global u  
     u = input("Username: ")
@@ -32,29 +41,56 @@ def log_in():
         print("Credentials Incorrect")
         log_in()
 
+#Display Money or dm scans the dicts to look for the code and then outputs the money in the account
+def dm():
+    code = int(input("Code: "))
+    if code == name_gp1["Code"]:
+        print(name_gp1["Money"])
+        menu()
+    elif code == name_gp2["Code"]:
+        print(name_gp2["Money"])
+        menu()
+    elif code == name_gp3["Code"]:
+        print(name_gp3["Money"])
+        menu()
+    
+    else:
+        print("Code doesn't exist")
+        dm()
+
+#Basic menu function, uses global varible u to allow the use of a name outside a def for the welcome
 def menu():
     print("Welcome " + str(u) + " to the online banking system")
-    print("""What would you like to do today:
+    display = input("""What would you like to do today:
 - Display money
 - Take out money
 - Deposit money
 """)
+    if display == "Display money" or "display money":
+        dm()
 
+    else:
+        menu()
 
+#Sign up scans dicts to see if the accounts are in old dicts and if they aren't it appends it to the new dict
 def sign_up():
     new_u = input("Enter new username: ")
     new_p = input("Enter new password: ")
-    if new_u in name_gp1["Username"] or new_u in name_gp2["Username"]:
+    new_c = input("Enter new code: ")
+    if new_u in name_gp1["Username"] or name_gp2["Username"]:
         print("Username exists")
         sign_up()
-    if new_p in name_gp1["Password"] or new_p in name_gp2["Password"]:
+    if new_p in name_gp1["Password"] or name_gp2["Password"]:
         print("Password exists")
         sign_up()
-            
+    if new_c in name_gp1["Code"] or name_gp2["Code"]:
+        print("Code exists")
+        sign_up()    
     else:
         name_gp3["Username"].append(new_u)
         name_gp3["Password"].append(new_p)
-        print("Username and password created")
+        name_gp3["Code"].append(new_c)
+        print("New Account Is Set-up")
         log_in()
 
 def options():
